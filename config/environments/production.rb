1f5ac0e736114d7e7781f956fd72a032f20e1509
Tanvi Jindal <jindal.tanvi@gmail.com>
2013-12-03 02:02:43 -0600

    added social share buttons in two places

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,14 +8,14 @@ Selfstarter::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  # Disable Rails's static asset server (Apache or nginx will already do this) (Tanvi: was false)
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
-  # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  # Don't fallback to assets pipeline if a precompiled asset is missed (Tanvi : was false)
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
