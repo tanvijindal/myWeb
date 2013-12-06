677dd436d5069a6e2099a72ac3bc2a1717314ba9
Tanvi Jindal <jindal.tanvi@gmail.com>
2013-12-06 01:29:46 -0600

    fixed alignment of share buttons

--- app/controllers/preorder_controller.rb
+++ app/controllers/preorder_controller.rb
@@ -2,8 +2,9 @@ class PreorderController < ApplicationController
   skip_before_action :verify_authenticity_token, :only => :ipn
 
   def prefill
-	@user_ip = request.remote_ip
+	#@user_ip = request.remote_ip
     @user = User.find_or_create_by(:email => params[:email], :ip => @user_ip)
+	#@user = User.find_or_create_by(:email => params[:email])
 	UserMailer.welcome_email(@user).deliver
 	
     redirect_to '/preorder/checkout'
