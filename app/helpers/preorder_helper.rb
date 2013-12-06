bea3b3d9400d1f7b3071b6fc8d36850eb729dc5a
Tanvi Jindal <jindal.tanvi@gmail.com>
2013-12-06 04:26:29 -0600

    stupid pinterest

--- app/helpers/preorder_helper.rb
+++ app/helpers/preorder_helper.rb
@@ -4,8 +4,8 @@ module PreorderHelper
   end
 
   def pin_it_button    
-      image_url = URI.encode("#{request.scheme}://#{request.host}#{image_path(Settings.product_image_path)}")
-      raw "<a href='http://pinterest.com/pin/create/button/?url=#{encoded_root_url}&media=#{image_url}&description=PikMoments%20%3A%20Capture%20amazing%20photographs%20automatically' data-pin-do=\"buttonPin\" data-pin-config=\"beside\"><img src='//assets.pinterest.com/images/pidgets/pinit_fg_en_rect_gray_20.png' /></a>"
+	image_url = URI.encode("#{request.scheme}://#{request.host}#{image_path(Settings.product_image_path)}")
+	raw "<a class=\"pin-it-button\" data-pin-do=\"buttonPin\" data-pin-config=\"beside\" data-pin-color=\"red\" always-show-count=\"true\" href=\"https://pinterest.com/pin/create/button/?url=#{encoded_root_url}&media=#{image_url}&description=PikMoments%20%3A%20Capture%20amazing%20photographs%20automatically\" > <img src=\"//assets.pinterest.com/images/pidgets/pin_it_button.png\" /></a>"
   end
   
   def tweet_button
