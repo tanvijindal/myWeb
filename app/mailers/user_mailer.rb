cc967a4cd738151c84bf88d14a803482a114d4c6
Tanvi Jindal <jindal.tanvi@gmail.com>
2013-12-21 21:57:52 -0600

    styles fixed

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -21,7 +21,7 @@ class UserMailer < ActionMailer::Base
 	def re_attempt_to_subscribe_mail(user)
 	  @user = user
 	  begin
-		mandrill = Mandrill::API.new 'GgbLEcHFNN8aiib6cxaW0w'
+		mandrill = Mandrill::API.new '2vnYmqdclHxJYUw_X2FAGg'
 		message = {"subject" => "You are already in the elite list for Pikmoments!",
 		  "text"=>"We are really happy to see you!",
 		  "from_email" => "pikmoments@gmail.com",
