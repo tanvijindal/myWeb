c6a1edcbf7b69b204a550089c71fa580a77bb3b6
Tanvi Jindal <jindal.tanvi@gmail.com>
2013-12-22 00:39:09 -0600

    mailer still

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Selfstarter::Application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.default_url_options = { :host => 'www.pikmoments.com' }
 
