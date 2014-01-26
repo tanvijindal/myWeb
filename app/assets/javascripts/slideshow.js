ad49637b39f7ca6ccab66f4d3c6b6ac6aaa56e92
Tanvi Jindal <jindal.tanvi@gmail.com>
2014-01-26 00:19:05 -0600

    Made the pagetab indicators on top slideshow circular and fixed their alignment issues.
    Signed-off-by: Tanvi Jindal <jindal.tanvi@gmail.com>

--- app/assets/javascripts/slideshow.js
+++ app/assets/javascripts/slideshow.js
@@ -1,10 +1,10 @@
 function next_slide() {
   var active = $('.slideshow .slides div.active');
-  var active_pagination_tab = $('.slideshow .slide-pagination .pagination-tabs div.active');
+  var active_pagination_tab = $('.slideshow .slide-pagination div.active');
   if (active.length == 0) active = $('.slideshow .slides div:last');
-  if (active_pagination_tab.length == 0) active_pagination_tab = $('.slideshow .slide-pagination .pagination-tabs div:last');
+  if (active_pagination_tab.length == 0) active_pagination_tab = $('.slideshow .slide-pagination div:last');
   var next = active.next().length ? active.next() : $('.slideshow .slides div:first');
-  var next_pagination_tab = active_pagination_tab.next().length ? active_pagination_tab.next() : $('.slideshow .slide-pagination .pagination-tabs div:first');
+  var next_pagination_tab = active_pagination_tab.next().length ? active_pagination_tab.next() : $('.slideshow .slide-pagination div:first');
   active.addClass('last-active');
   next.css({opacity: 0.0}).addClass('active').animate({opacity: 1.0}, 1000, function() {
 													  active.removeClass('active last-active');
@@ -19,11 +19,11 @@ function manual_slide_nav(event) {
   clearTimeout(timeout_var);
   var i = event.data.val;
   var active = $('.slideshow .slides div.active');
-  var active_pagination_tab = $('.slideshow .slide-pagination .pagination-tabs div.active');
+  var active_pagination_tab = $('.slideshow .slide-pagination div.active');
   if (active.length == 0) active = $('.slideshow .slides div:last');
-  if (active_pagination_tab.length == 0) active_pagination_tab = $('.slideshow .slide-pagination .pagination-tabs div:last');
+  if (active_pagination_tab.length == 0) active_pagination_tab = $('.slideshow .slide-pagination div:last');
   var next = $('.slideshow .slides').children('div').eq(i);
-  var next_pagination_tab = $('.slideshow .slide-pagination .pagination-tabs').children('div').eq(i);
+  var next_pagination_tab = $('.slideshow .slide-pagination').children('div').eq(i);
   
   next.css({opacity: 0.0}).addClass('active').animate({opacity: 1.0}, 1000, function() {
 													  active.removeClass('active last-active');
@@ -36,7 +36,7 @@ function manual_slide_nav(event) {
 
 $(function() {
   timeout_var = setTimeout(next_slide, 5000);
-  tab_buttons =  $('.slideshow > .slide-pagination > .pagination-tabs div');
+  tab_buttons =  $('.slideshow > .slide-pagination div');
   for (i=0; i<3; i++ )
   {
 	tab_buttons.eq(i).on('click', {val : i}, manual_slide_nav );
