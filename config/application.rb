68ea641b0ad6aaf9e75e7e1059eb8b4a7759fd79
Arpit <arpitagarwal2901@gmail.com>
2014-01-28 03:03:21 -0600

    placed images for mobile

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module Selfstarter
     # Enable the asset pipeline
     config.assets.enabled = true
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '2.0'
+    config.assets.version = '1.0'
     # --- Standard Rails Config ---
   end
 end
