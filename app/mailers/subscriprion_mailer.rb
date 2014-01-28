e762a8f4bd30517e097fed245644228271f436b5
Arpit <arpitagarwal2901@gmail.com>
2014-01-28 03:17:50 -0600

    error in subscription mailer fixed

--- app/mailers/subscriprion_mailer.rb
+++ app/mailers/subscriprion_mailer.rb
@@ -1,16 +1,2 @@
-require 'mandrill'
 
-class SubscriptionMailer < MandrillMailer::TemplateMailer
-  default from: 'info@pikmoments.com'
-  def welcome_template(user)
-    @user = user
-    mandrill_mail template: 'trial',
-    subject: 'Welcome to Pikmoments',
-    to: @user.email,
-    important: true,
-    inline_css: true,
-    end
-  end
-
-end
 
