79748da1f738293141b0a7f4bbcf2dc73a34c358
Arpit <arpitagarwal2901@gmail.com>
2014-01-27 21:32:51 -0600

    Positioning of battery and hours animation

--- app/assets/javascripts/battery_animation.js
+++ app/assets/javascripts/battery_animation.js
@@ -14,12 +14,31 @@ function battery_animation() {
     }).blur(function () {
         clearInterval(animation); // Clearing interval on window blur
     });
+
+    //Animate the number of hours
+
+    $({someValue: 0}).animate({someValue: 20}, {
+      duration: 2000,
+      easing:'swing', // can be anything
+      step: function() { // called on every step
+          // Update the element's text with rounded-up value:
+          $('#battery_hours').text(commaSeparateNumber(Math.round(this.someValue)));
+      }
+  });
+
     timeout = setTimeout(battery_animation, 5000);
+}
+
+function commaSeparateNumber(val){
+	while (/(\d+)(\d{3})/.test(val.toString())){
+      val = val.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+    }
+    return val;
 }
 
 $(document).ready(function() {
 	var full = document.getElementById('full');
 	var empty = document.getElementById('empty');
 	full.style.display = "none";
-  	timeout = setTimeout(battery_animation, 5000);
+  	timeout = setTimeout(battery_animation, 10);
 });
