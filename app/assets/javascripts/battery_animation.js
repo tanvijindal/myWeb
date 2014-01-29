df3f61da87c2f1debe2947480c8a19c2099b5f9c
Arpit <arpitagarwal2901@gmail.com>
2014-01-29 04:54:50 -0600

    Removed green animation, ajax for phone fixed

--- app/assets/javascripts/battery_animation.js
+++ app/assets/javascripts/battery_animation.js
@@ -1,29 +1,13 @@
 function battery_animation() {
-	var full = document.getElementById('full');
-	full.style.display = "block";
-	full.style.width = "0px";
-	full.style.height = "10px";
-	var animation = setInterval(function(){
-    
-    if(parseInt(full.style.width,10) == 1260)
-        clearInterval(animation);
-    full.style.width = parseInt(full.style.width,10)+10 +"px";
-	},20);
-	$(window).focus(function () {
-        clearInterval(animation); // Clearing interval if for some reason it has not been cleared yet
-    }).blur(function () {
-        clearInterval(animation); // Clearing interval on window blur
-    });
+  //Animate the number of hours
 
-    //Animate the number of hours
-
-    $({someValue: 0}).animate({someValue: 20}, {
-      duration: 3000,
-      easing:'swing', // can be anything
-      step: function() { // called on every step
-          // Update the element's text with rounded-up value:
-          $('#battery_hours').text(commaSeparateNumber(Math.round(this.someValue)));
-      }
+  $({someValue: 0}).animate({someValue: 20}, {
+    duration: 3000,
+    easing:'swing', // can be anything
+    step: function() { // called on every step
+      // Update the element's text with rounded-up value:
+      $('#battery_hours').text(commaSeparateNumber(Math.round(this.someValue)));
+    }
   });
 
     timeout = setTimeout(battery_animation, 6000);
@@ -37,6 +21,5 @@ function commaSeparateNumber(val){
 }
 
 $(document).ready(function() {
-	var full = document.getElementById('full');
   timeout = setTimeout(battery_animation, 10);
 });
--- app/assets/javascripts/subscribe.js
+++ app/assets/javascripts/subscribe.js
@@ -3,4 +3,8 @@ $(function() {
 								{
 								$('input[name="email"]').val("Thanks for subscribing");
 								});
+  $(".footer #subscribe_email_form").on("ajax:success", function()
+								{
+								$('input[name="email"]').val("Thanks for subscribing");
+								});
 });
