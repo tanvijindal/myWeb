d6c6d51d82f3edbc6acabe0a7037dcb03e5dcbe5
Tanvi Jindal <jindal.tanvi@gmail.com>
2014-01-29 04:56:36 -0600

    subscription email

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -1,2 +1,13 @@
-
-
+class SubscriptionMailer < MandrillMailer::TemplateMailer
+  default from: 'info@pikmoments.com'
+  
+  def welcome(user)
+	@user = user
+	
+    mandrill_mail :template => 'WebEmail',
+	:subject => 'PikMoments: Capture amazing moments automatically',
+	:to => @user.email,
+	important: true,
+	inline_css: true
+  end
+end
